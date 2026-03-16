2dddd93cb51b644939b750efea7f35046fffdd3e
BilalQanoa <bilalqanoa@gmail.com>
2026-03-16 14:00:31 +0200

    Upload sarching an array code

---
+++ searching_array.js
@@ -0,0 +1,16 @@
+var numbers = [5, 10, 15, 20, 25];
+
+var index = -1;
+for (var i = 0; i < numbers.length; i++){
+    if (numbers[i] == 25){
+        index = i;
+        break;
+    }
+}
+
+if (index != -1){
+    console.log("Found at position: "+index);
+} else {
+    console.log("Not Found");
+    
+}
