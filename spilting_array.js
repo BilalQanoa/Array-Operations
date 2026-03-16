0612d10040c2057e2cd5b4b43c38327356de07d9
BilalQanoa <bilalqanoa@gmail.com>
2026-03-16 14:32:09 +0200

    upload spilting array code

---
+++ spilting_array.js
@@ -0,0 +1,9 @@
+var items = ["a", "b", "c", "d", "e"];
+
+var firstArray = items.slice(0, 3);
+var secondArray = items.slice(3);
+
+console.log(items);
+
+console.log(firstArray);
+console.log(secondArray);
