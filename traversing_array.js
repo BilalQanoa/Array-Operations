6cf626885de11a82690e6b5f5fcbbc1aa8ef10a7
BilalQanoa <bilalqanoa@gmail.com>
2026-03-16 13:38:40 +0200

    upload Traversing an Array code

---
+++ traversing_array.js
@@ -0,0 +1,11 @@
+var numbers = [10, 20, 30, 40, 50];
+
+for (var i = 0; i <= numbers.length - 1; i++){
+    console.log(numbers[i]);   
+}
+
+console.log("--------------------------");
+
+for (var i = numbers.length - 1; i >= 0 ; i--) {
+    console.log(numbers[i]);
+}
